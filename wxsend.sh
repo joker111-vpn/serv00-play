11121c95192d4b7992ac086fa09bfbdb0d0f199b
joker111-vpn <duzhihui111@gmail.com>
2025-02-27 09:44:53 +0800

    Update wxsend.sh

--- wxsend.sh
+++ wxsend.sh
@@ -6,7 +6,7 @@ sendKey=${WXSENDKEY}
 title="msg_from_serv00-play"
 URL="https://sctapi.ftqq.com/$sendKey.send?"
 
-if [[ -z ${sendKey} ]]; then
+if [[ -z ${SCT271199Tz5rChH5TXzYZeJwtNyJS8K31} ]]; then
   echo "未配置微信推送的sendKey,通过 https://sct.ftqq.com/r/13223 注册并登录server酱，取得sendKey"
 else
   res=$(timeout 20s curl -s -X POST $URL -d title=${title} -d desp="${text}")
